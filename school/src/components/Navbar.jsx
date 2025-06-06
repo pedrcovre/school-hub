b96b2c25c8e507eaf18b235e52b1edfdb4ccca87
Pedro de Jesus Covre <pedro.j.covre@gmail.com>
2025-06-05 23:36:43 -0300

    polindo codigo

--- school/src/components/Navbar.jsx
+++ school/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Header = () => {
       <div className='max-w-screen-2xl mx-auto px-5 flex items-center justify-between h-full'>
         <div className='flex items-center gap-5'>
           <a href='/'>
-            <img src='/logo.svg' width={50} height={50} alt='School Hub' />
+            <img src='/logo.svg' className="w-[50px]" alt='School Hub' />
           </a>
           <h1 className='text-3xl font-bold'>
             <a href='#'>School Hub</a>
@@ -26,20 +26,20 @@ const Header = () => {
         </div>
         <div className='flex items-center gap-6'>
           <form className='flex items-center bg-[#395F8B1A] rounded-lg px-3 w-[263px] h-[50px]'>
-            <img src='/search.svg' alt='search' width={25} height={25} />
+            <img src='/search.svg' alt='search' className="w-[25px]" />
             <input
               type='text'
               placeholder='Buscar'
               className='ml-3 w-full bg-transparent text-zinc-950 text-base focus:outline-none'
             />
           </form>
-          <button type="submit" className='flex items-center justify-center bg-[#395F8B1A] rounded-lg px-3 w-[50px] h-[50px]'>
+          <button type="button" className='flex items-center justify-center bg-[#395F8B1A] rounded-lg px-3 w-[50px] h-[50px]'>
             <span className='material-symbols-rounded text-zinc-950'>
               notifications
             </span>
           </button>
-          <button type="submit">
-          <img src='/icon-avatar-aluno.png' width={50} alt='Avatar' />
+          <button type="button">
+          <img src='/icon-avatar-aluno.png' className="w-[50px]" alt='Avatar' />
           </button>
         </div>
       </div>
