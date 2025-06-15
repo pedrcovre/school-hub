bb0cb61643480205df2d21403406081117e5e2c8
Bernardo Silva <bernardo.silva2345@gmail.com>
2025-06-15 04:08:48 -0300

    request banco e back

--- school/auth-api/server.js
+++ school/auth-api/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 
 const authRoutes = require('./routes/authRoutes');
-const requestRoutes = require('./routes/requestRoutes'); 
+const requestRoutes = require('./routes/requestRoute');
 
 app.use(cors());
 app.use(express.json());
