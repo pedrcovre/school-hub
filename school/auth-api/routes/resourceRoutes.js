d0d8020b1905f1aa0b509786a7fa4876f40f2bb9
Pedro Covre <pedro.j.covre@gmail.com>
2025-06-19 15:48:19 -0300

    Atualizar o resourceRoutes.js

--- school/auth-api/routes/resourceRoutes.js
+++ school/auth-api/routes/resourceRoutes.js
@@ -9,23 +9,23 @@ const {
   deleteResource
 } = require('../controllers/resourcesController')
 
-// Cria recurso com upload de arquivo opcional
+
 router.post(
   '/',
   authMiddleware,
-  adminMiddleware, // só admin pode adicionar recurso
+  adminMiddleware,
   upload.single('resourceFile'),
   createResource
 )
 
-// Listar recursos de uma requisição específica
+
 router.get(
   '/request/:requestId',
   authMiddleware,
   getResourcesByRequestId
 )
 
-// Excluir recurso (opcional)
+
 router.delete('/:id', authMiddleware, adminMiddleware, deleteResource)
 
 module.exports = router
